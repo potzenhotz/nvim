f78bd8287f5a301c3eb24cb2579b77ff882b4e9b
Lukas Müßle <lukas.mussle@MBP-von-admin.fritz.box>
2024-06-12 16:42:14 +0200

    add further stuff like mini and keymaps and recipies

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,11 @@ local map = LazyVim.safe_keymap_set
 
 -- Famouse esc sequence
 map("i", "jk", "<Esc>")
+
+-- Perusing code faster with K and J
+map({ "n", "v" }, "K", "5k", { noremap = true, desc = "Up faster" })
+map({ "n", "v" }, "J", "5j", { noremap = true, desc = "Down faster" })
+
+-- Remap K and J
+map({ "n", "v" }, "<leader>k", "K", { noremap = true, desc = "Keyword" })
+map({ "n", "v" }, "<leader>j", "J", { noremap = true, desc = "Join lines" })
