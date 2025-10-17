456894a1333b3751bcba6677e17c6a252ac425c1
Lukas Müßle <lukas.mussle@MacBookPro.fritz.box>
2025-10-17 10:07:16 +0200

    change smearness

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -4,6 +4,13 @@ return {
   opts = {
     -- Smear cursor color. Defaults to Cursor GUI color if not set.
     -- Set to "none" to match the text color at the target cursor position.
-    cursor_color = "#d3cdc3",
+    cursor_color = "#8D148F",
+    stiffness = 0.3,
+    trailing_stiffness = 0.1,
+    damping = 0.5,
+    trailing_exponent = 5,
+    never_draw_over_target = true,
+    hide_target_hack = true,
+    gamma = 1,
   },
 }
