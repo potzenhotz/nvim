58a58ed4b304633254f910dc7208b2b435bbc333
Lukas Müßle <lukas.mussle@MacBookPro.fritz.box>
2025-10-17 08:40:19 +0200

    add smear cursor and adapt dap

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,11 +1,16 @@
-local dap = require("dap")
-dap.configurations.python = {
-  {
-    justMyCode = false,
-    type = "python",
-    request = "launch",
-    name = "Launch file",
-    program = "${file}",
-    console = "integratedTerminal",
-  },
+return {
+  "mfussenegger/nvim-dap",
+  config = function()
+    local dap = require("dap")
+    dap.configurations.python = {
+      {
+        justMyCode = false,
+        type = "python",
+        request = "launch",
+        name = "Launch file",
+        program = "${file}",
+        console = "integratedTerminal",
+      },
+    }
+  end,
 }
---
+++ lua/plugins/smear-cursor.lua
@@ -0,0 +1,9 @@
+return {
+  "sphamba/smear-cursor.nvim",
+
+  opts = {
+    -- Smear cursor color. Defaults to Cursor GUI color if not set.
+    -- Set to "none" to match the text color at the target cursor position.
+    cursor_color = "#d3cdc3",
+  },
+}
