9e86315067e020f400830ee0f771e9dd61ecb367
Lukas Müßle <lukas.mussle@MacBookPro.fritz.box>
2026-01-29 11:25:28 +0100

    remove neotest config back to default and add smear cursor

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -6,8 +6,8 @@ return {
     -- Set to "none" to match the text color at the target cursor position.
     cursor_color = "#8D148F",
     stiffness = 0.3,
-    trailing_stiffness = 0.1,
-    damping = 0.5,
+    trailing_stiffness = 0.15,
+    damping = 0.85,
     trailing_exponent = 5,
     never_draw_over_target = true,
     hide_target_hack = true,
